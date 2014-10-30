f0b5b8eee8f93173405dbaeeb3a50f053e4925b9
wyleung <w.y.leung@e-sensei.nl>
2014-10-30 17:01:17 +0100

    Setup the installer and package meta info

---
+++ setup.py
@@ -0,0 +1,44 @@
+import sys
+from setuptools import setup
+
+if sys.version_info < (2, 6):
+    raise Exception('pySVtools requires Python 2.6 or higher.')
+
+# Todo: How does this play with pip freeze requirement files?
+requires = ['pyvcf']
+
+# Python 2.6 does not include the argparse module.
+try:
+    import argparse
+except ImportError:
+    requires.append('argparse')
+
+import pysvtools as distmeta
+
+setup(
+    name='pysvtools',
+    version=distmeta.__version__,
+    description='VCF toolkit for SV analysis',
+    long_description=distmeta.__doc__,
+    author=distmeta.__author__,
+    author_email=distmeta.__contact__,
+    url=distmeta.__homepage__,
+    license='MIT License',
+    platforms=['any'],
+    packages=['pysvtools'],
+    install_requires=requires,
+    entry_points = {
+        'console_scripts': [
+            'mergevcf = pysvtools.merge:main'
+        ]
+    },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+    ],
+    keywords='bioinformatics'
+)
